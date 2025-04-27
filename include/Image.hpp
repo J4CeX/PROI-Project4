5ec7360cd04142571ae569e245765d3b8ca5c450
Jacek <jacek_kozakowski@wp.pl>
2025-04-27 14:33:42 +0200

    Implemented the rule of five and iterator in Image class

--- include/Image.hpp
+++ include/Image.hpp
@@ -1,15 +1,40 @@
 #pragma once
-#include <vector>
 #include "Element.hpp"
+#include <vector>
 
 namespace SVG {
     class Image {
         private:
             std::vector<Element> elements;
+            int size;
         public:
-            Image();
+            Image() = delete;
+            Image(std::vector<Element> elements)
+                : elements(elements), size(elements.size()) {}
+            Image(const Image& secImage);
             Image(Image&& secImage) noexcept;
-            Image& operator=(const Image& secImage) noexcept;
-            ~Image();
+            int getSize() const { return size; }
+            void addElement(const Element& newElement);
+            void removeElement(const Element& rmElement);
+            Element& operator[](const int& index);
+            Image& operator=(const Image& secImage);
+            Image& operator=(const Image&& secImage) noexcept;
+            ~Image() {};
+
+            class Iterator {
+                private:
+                    std::vector<Element>::iterator current;
+                public:
+                    Iterator(std::vector<Element>::iterator it): current(it) {}
+                    Element operator*() const { return *current; }
+                    Iterator& operator++() {
+                        ++current;
+                        return *this;
+                    }
+                    bool operator!=(const Iterator& secIterator) const { return current != secIterator.current; }
+                    std::vector<Element>::iterator getCurrent() const { return current; }
+                };
+            Iterator begin() { return Iterator(elements.begin()); }
+            Iterator end() { return Iterator(elements.end()); }
     };
 }
--- src/Image.cpp
+++ src/Image.cpp
@@ -1,1 +1,54 @@
 #include "Image.hpp"
+
+using namespace SVG;
+
+Image::Image(const Image& secImage) {
+    size = secImage.size;
+    elements = secImage.elements;
+}
+
+Image::Image(Image&& secImage) noexcept {
+    size = secImage.size;
+    elements = std::move(secImage.elements);
+}
+
+void Image::addElement(const Element& newElement) {
+    elements.push_back(newElement);
+    size++;
+}
+
+void Image::removeElement(const Element& rmElement) {
+    for (Image::Iterator it = begin(); it != end(); ++it) {
+        if (*it == rmElement) {
+            elements.erase(it.getCurrent());
+            size--;
+            break;
+        }
+    }
+}
+
+Element& Image::operator[](const int& index) {
+    if (index < size && index >= 0) {
+        return elements[index];
+    }
+    else {
+        std::cout << "\aIndex out of range!" << std::endl;
+        return;
+    }
+}
+
+Image& Image::operator=(const Image& secImage) {
+    if (this != &secImage) {
+        size = secImage.size;
+        elements = secImage.elements;
+    }
+    return *this;
+}
+
+Image& Image::operator=(const Image&& secImage) noexcept {
+    if (this != &secImage) {
+        size = secImage.size;
+        elements = std::move(secImage.elements);
+    }
+    return *this;
+}
