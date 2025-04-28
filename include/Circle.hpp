422083d4236bdf4c8c263835198ee3b843d0b4ae
Jacek <jacek_kozakowski@wp.pl>
2025-04-28 14:24:19 +0200

    Fixed Circle and Implemented tests for Circle

--- include/Circle.hpp
+++ include/Circle.hpp
@@ -6,11 +6,10 @@ namespace SVG {
         private:
             int radius;
         public:
-            Circle(int width, int height, int radius,
-                int x = 0, int y = 0,
+            Circle(const int& radius, const int& x = 0, const int& y = 0,
                 const std::string& fill = "none",
                 const std::string& stroke = "none")
-                : Element(x, y, width, height, fill, stroke),
+                : Element(x, y, 0, 0, fill, stroke),
                 radius(radius) {};
             int getRadius() const { return radius; }
             bool operator==(const Circle& secCircle) const;
--- tests/CircleTests.cpp
+++ tests/CircleTests.cpp
@@ -1,1 +1,45 @@
 #include <gtest/gtest.h>
+#include "Circle.hpp"
+
+using SVG::Circle;
+
+TEST(CircleTests, CircleConstructor) {
+    Circle circle(10, 20, 30, "red", "blue");
+    ASSERT_EQ(circle.getX(), 20);
+    ASSERT_EQ(circle.getY(), 30);
+    ASSERT_EQ(circle.getRadius(), 10);
+    ASSERT_EQ(circle.getFill(), "red");
+    ASSERT_EQ(circle.getStroke(), "blue");
+}
+
+TEST(CircleTests, CircleDefaultValuesConstructor) {
+    Circle circle(10);
+    ASSERT_EQ(circle.getX(), 0);
+    ASSERT_EQ(circle.getY(), 0);
+    ASSERT_EQ(circle.getRadius(), 10);
+    ASSERT_EQ(circle.getFill(), "none");
+    ASSERT_EQ(circle.getStroke(), "none");
+}
+
+TEST(CircleTests, CircleCopy) {
+    Circle circle(10, 20, 30, "red", "blue");
+    Circle secCircle(1, 2, 3, "green", "yellow");
+    circle = secCircle;
+    ASSERT_EQ(circle.getX(), 2);
+    ASSERT_EQ(circle.getY(), 3);
+    ASSERT_EQ(circle.getRadius(), 1);
+    ASSERT_EQ(circle.getFill(), "green");
+    ASSERT_EQ(circle.getStroke(), "yellow");
+    Circle thCircle = secCircle;
+    ASSERT_EQ(thCircle.getX(), 2);
+    ASSERT_EQ(thCircle.getY(), 3);
+    ASSERT_EQ(thCircle.getRadius(), 1);
+    ASSERT_EQ(thCircle.getFill(), "green");
+    ASSERT_EQ(thCircle.getStroke(), "yellow");
+}
+
+TEST(CircleTests, CirclePrint) {
+    Circle circle(10, 20, 30, "red", "blue");
+    std::string expectedResult = "<circle cx=\"20\" cy=\"30\" r=\"10\" fill=\"red\" stroke=\"blue\" />";
+    ASSERT_EQ(circle.print(), expectedResult);
+}
