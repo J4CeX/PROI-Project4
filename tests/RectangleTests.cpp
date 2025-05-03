a1601a4f512426692a2660dee28a24333b46baa3
Jacek <jacek_kozakowski@wp.pl>
2025-05-03 18:42:41 +0200

    Implemented new tests for Rectangle and Text classes

--- tests/RectangleTests.cpp
+++ tests/RectangleTests.cpp
@@ -47,3 +47,35 @@ TEST(RectangleTests, RectanglePrint) {
     std::string expectedResult = "<rect x=\"30\" y=\"40\" width=\"10\" height=\"20\" fill=\"red\" stroke=\"blue\" />";
     ASSERT_EQ(rectangle.print(), expectedResult);
 }
+
+TEST(RectangleTests, RectangleSetters) {
+    Rectangle rectangle(10, 20, 30, 40, "red", "blue");
+    ASSERT_EQ(rectangle.getX(), 30);
+    ASSERT_EQ(rectangle.getY(), 40);
+    ASSERT_EQ(rectangle.getWidth(), 10);
+    ASSERT_EQ(rectangle.getHeight(), 20);
+    ASSERT_EQ(rectangle.getFill(), "red");
+    ASSERT_EQ(rectangle.getStroke(), "blue");
+    rectangle.setX(1);
+    rectangle.setY(2);
+    rectangle.setWidth(3);
+    rectangle.setHeight(4);
+    rectangle.setFill("green");
+    rectangle.setStroke("yellow");
+    ASSERT_EQ(rectangle.getX(), 1);
+    ASSERT_EQ(rectangle.getY(), 2);
+    ASSERT_EQ(rectangle.getWidth(), 3);
+    ASSERT_EQ(rectangle.getHeight(), 4);
+    ASSERT_EQ(rectangle.getFill(), "green");
+    ASSERT_EQ(rectangle.getStroke(), "yellow");
+}
+
+TEST(RectangleTests, RectangleComparison) {
+    Rectangle firstRectangle(10, 20, 30, 40, "red", "blue");
+    Rectangle secondRectangle(1, 2, 3, 4, "green", "white");
+    ASSERT_TRUE(firstRectangle != secondRectangle);
+    ASSERT_FALSE(firstRectangle == secondRectangle);
+    secondRectangle = firstRectangle;
+    ASSERT_TRUE(firstRectangle == secondRectangle);
+    ASSERT_FALSE(firstRectangle != secondRectangle);
+}
--- tests/TextTests.cpp
+++ tests/TextTests.cpp
@@ -9,6 +9,7 @@ TEST(TextTests, TextConstructor) {
     ASSERT_EQ(text.getX(), 10);
     ASSERT_EQ(text.getY(), 20);
     ASSERT_EQ(text.getFontSize(), 30);
+    ASSERT_EQ(text.getTextAnchor(), "middle");
     ASSERT_EQ(text.getFill(), "red");
     ASSERT_EQ(text.getStroke(), "blue");
 }
@@ -19,6 +20,7 @@ TEST(TextTests, TextDefaultValuesConstructor) {
     ASSERT_EQ(text.getX(), 0);
     ASSERT_EQ(text.getY(), 0);
     ASSERT_EQ(text.getFontSize(), 16);
+    ASSERT_EQ(text.getTextAnchor(), "start");
     ASSERT_EQ(text.getFill(), "none");
     ASSERT_EQ(text.getStroke(), "none");
 }
@@ -31,6 +33,7 @@ TEST(TextTests, TextCopy) {
     ASSERT_EQ(text.getX(), 1);
     ASSERT_EQ(text.getY(), 2);
     ASSERT_EQ(text.getFontSize(), 3);
+    ASSERT_EQ(text.getTextAnchor(), "end");
     ASSERT_EQ(text.getFill(), "green");
     ASSERT_EQ(text.getStroke(), "yellow");
     Text thText = text;
@@ -38,6 +41,7 @@ TEST(TextTests, TextCopy) {
     ASSERT_EQ(thText.getX(), 1);
     ASSERT_EQ(thText.getY(), 2);
     ASSERT_EQ(thText.getFontSize(), 3);
+    ASSERT_EQ(text.getTextAnchor(), "end");
     ASSERT_EQ(thText.getFill(), "green");
     ASSERT_EQ(thText.getStroke(), "yellow");
 }
@@ -46,4 +50,40 @@ TEST(TextTests, TextPrint) {
     Text text("some text", 10, 20, 30, "middle", "red", "blue");
     std::string expectedResult = "<text x=\"10\" y=\"20\" font-size=\"30\" text-anchor=\"middle\" fill=\"red\" stroke=\"blue\">some text</text>";
     ASSERT_EQ(text.print(), expectedResult);
+}
+
+TEST(TextTests, TextSetters) {
+    Text text("some text", 10, 20, 30, "middle", "red", "blue");
+    ASSERT_EQ(text.getContent(), "some text");
+    ASSERT_EQ(text.getX(), 10);
+    ASSERT_EQ(text.getY(), 20);
+    ASSERT_EQ(text.getFontSize(), 30);
+    ASSERT_EQ(text.getTextAnchor(), "middle");
+    ASSERT_EQ(text.getFill(), "red");
+    ASSERT_EQ(text.getStroke(), "blue");
+    text.setX(1);
+    text.setY(2);
+    text.setFontSize(3);
+    text.setTextAnchor("end");
+    text.setFill("green");
+    text.setStroke("yellow");
+    text.setContent("different text");
+    ASSERT_EQ(text.getContent(), "different text");
+    ASSERT_EQ(text.getX(), 1);
+    ASSERT_EQ(text.getY(), 2);
+    ASSERT_EQ(text.getFontSize(), 3);
+    ASSERT_EQ(text.getTextAnchor(), "end");
+    ASSERT_EQ(text.getFill(), "green");
+    ASSERT_EQ(text.getStroke(), "yellow");
+
+}
+
+TEST(TextTests, TextComparison) {
+    Text firstText("some text", 10, 20, 30, "middle", "red", "blue");
+    Text secondText("some text 2", 1, 2, 3, "end", "yellow", "white");
+    ASSERT_TRUE(firstText != secondText);
+    ASSERT_FALSE(firstText == secondText);
+    secondText = firstText;
+    ASSERT_TRUE(firstText == secondText);
+    ASSERT_FALSE(firstText != secondText);
 }
