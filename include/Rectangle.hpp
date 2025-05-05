7f3d84b2d4d5a61ae0fadd23719a03b6265c4400
Jacek <jacek_kozakowski@wp.pl>
2025-05-05 13:24:59 +0200

    Implemented operator== for Rectangle

--- include/Rectangle.hpp
+++ include/Rectangle.hpp
@@ -9,10 +9,10 @@ namespace SVG {
                 const std::string& fill = "none",
                 const std::string& stroke = "none")
                 : Element(x, y, width, height, fill, stroke) {};
-                std::string print() const override;
-                void edit() override;
-                bool operator==(const Element& secElement) const override;
-                bool operator!=(const Element& secElement) const override { return !(*this == secElement); }
-                std::unique_ptr<Element> clone() const override { return std::make_unique<Rectangle>(*this); }
+            std::string print() const override;
+            void edit() override;
+            bool operator==(const Element& secElement) const override;
+            bool operator!=(const Element& secElement) const override { return !(*this == secElement); }
+            std::unique_ptr<Element> clone() const override { return std::make_unique<Rectangle>(*this); }
     };
 }
